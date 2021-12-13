1b5d6b6bf7045aa6a1a84bec6f550fa62c79c4ca
Sechma <marokac@seznam.cz>
2021-12-13 21:49:00 +0100

    almosr done this shit

--- src/main.py
+++ src/main.py
@@ -6,20 +6,21 @@
 
 from scipy.io import wavfile
 import numpy as np
+from matplotlib import pyplot as plt
 from scipy.signal import spectrogram, lfilter, freqz, tf2zpk
+import cmath
+import copy
+
 
 def dft(array):
-    fill = numpy.zeros(1024-len(array))
-    array = numpy.append(array, fill)
-    dft_arr = []
-    arr_len = len(array)
+    dft_arr =  np.ndarray((1024))
+    arr_len = array.size
     for i in range(arr_len):
         coef = 0
         for j in range(arr_len):
             coef += array[j] * cmath.exp(-2j * cmath.pi * i * j * (1 / arr_len))
-        dft_arr.append(coef)
+        np.append(dft_arr,coef,axis=0)
     return dft_arr
-
 
 #4.1
 fs, data = wavfile.read("../audio/xsechr00.wav")
@@ -43,6 +44,83 @@ if( data.max() >= data.min() ):
 else:
     data = data / abs(data.min())
 
-# 45264 / 1024 == 45
-frames = numpy.ndarray(( , ))
+frame_size = 1024 
+number_of_frames =  data.size//512
+# arr = np.ndarray((radek,sloupec))
+
+begin = 0
+end = 1024
+offset = 512
+frames = np.ndarray((87,1024)) #number of frames, length frame
+
+for i in range(0,87):
+    val = data[begin:end]
+    for j in range(0,1024):
+        frames[i][j] = val[j]
+    begin+=offset
+    end+=offset
+        
+frame = frames[4]
+
+myrange = np.arange(1024)
+plt.figure(figsize=(6,3))
+plt.gca().set_xlabel('t [s]')
+plt.gca().set_title('jeden rámec')
+plt.plot(myrange/fs, frame, label="signál")
+plt.legend()
+plt.tight_layout()
+plt.grid()
+plt.show()
+#*****************************************************
 #4.3
+"""
+frame_dft = dft(frame)
+
+myrange = np.arange(1024)
+plt.figure(figsize=(6,3))
+plt.gca().set_xlabel('t [s]')
+plt.gca().set_title('jeden dtft rámec')
+plt.plot(myrange/fs, frame_dft, label="DFT")
+plt.legend()
+plt.tight_layout()
+plt.grid()
+plt.show()
+
+fft_frame = np.fft.fft(frame)
+myrange = np.arange(1024)
+plt.figure(figsize=(6,3))
+plt.gca().set_xlabel('t [s]')
+plt.gca().set_title('jeden dtft rámec')
+plt.plot(myrange/fs, fft_frame, label="DFT")
+plt.legend()
+plt.tight_layout()
+plt.grid()
+plt.show()"""
+#**********************************************************
+# 4.4
+frames2 = copy.deepcopy(frames)
+DFT = []
+DFT_log = []
+
+for k in range(0,87): # jednotlivé rámce
+    frames2[k] = np.pad(frames2[k], (0, 1024 - len(frames2[k])), 'constant')
+    N = len(frames2[k])
+    kof = np.fft.fft(frames2[k])
+    DFT.append(kof)
+    DFT_log.append(kof)
+    res_val = 10 * np.log10(np.abs(DFT[k])**2)
+    DFT_log[k] = res_val
+    DFT[k] = DFT[k][:512]
+    DFT_log[k] = DFT_log[k][:512]
+
+
+plt.figure(figsize=(12,6))
+plt.imshow(np.rot90(DFT_log), extent=[0, 1, 0, fs/2], aspect='auto')
+plt.gca().set_title('DFT')
+plt.gca().set_xlabel('t [s]')
+plt.gca().set_ylabel('f [Hz]')
+b = plt.colorbar()
+b.set_label('Spektralní hustota výkonu [dB]', rotation=270,labelpad=16)
+plt.tight_layout()
+plt.show()
+
