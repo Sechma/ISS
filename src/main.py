97ef596dbe81f108466f8fc96612e10fab4af0c7
Sechma <marokac@seznam.cz>
2021-12-20 22:36:13 +0100

    4.9 only missing

--- src/main.py
+++ src/main.py
@@ -4,22 +4,22 @@
 #Created at: 5.12.2021
 #@author: xsechr00
 
+
 from scipy.io import wavfile
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.signal import spectrogram, lfilter, freqz, tf2zpk
 import cmath
-import copy
-
+import IPython
 
 def dft(array):
-    dft_arr =  np.ndarray((1024))
+    dft_arr = []
     arr_len = array.size
     for i in range(arr_len):
         coef = 0
         for j in range(arr_len):
             coef += array[j] * cmath.exp(-2j * cmath.pi * i * j * (1 / arr_len))
-        np.append(dft_arr,coef,axis=0)
+        dft_arr.append(coef)
     return dft_arr
 
 #4.1
@@ -31,18 +31,11 @@ print("Delka signalu", time,"[s]")
 print("MAX: ",data.max())
 print("MIN:",data.min())    
 #todo zobrazit na osu
-     
+
 #4.2
-mid = 0
-for i in range(0,data.size):
-    mid += data[i]
 
-mid = mid / data.size
-
-if( data.max() >= data.min() ):
-    data = data / data.max()
-else:
-    data = data / abs(data.min())
+data = data - np.mean(data)
+data = data / np.abs(data).max()
 
 frame_size = 1024 
 number_of_frames =  data.size//512
@@ -71,12 +64,13 @@ plt.legend()
 plt.tight_layout()
 plt.grid()
 plt.show()
+
 #*****************************************************
 #4.3
-"""
-frame_dft = dft(frame)
 
-myrange = np.arange(1024)
+frame_dft = dft(frame)
+frame_dft = frame_dft[0:512]
+myrange = np.arange(512)
 plt.figure(figsize=(6,3))
 plt.gca().set_xlabel('t [s]')
 plt.gca().set_title('jeden dtft rámec')
@@ -87,40 +81,158 @@ plt.grid()
 plt.show()
 
 fft_frame = np.fft.fft(frame)
-myrange = np.arange(1024)
+fft_frame = fft_frame[0:512]
+myrange = np.arange(512)
 plt.figure(figsize=(6,3))
 plt.gca().set_xlabel('t [s]')
-plt.gca().set_title('jeden dtft rámec')
-plt.plot(myrange/fs, fft_frame, label="DFT")
+plt.gca().set_title('jeden fft rámec')
+plt.plot(myrange/fs, fft_frame, label="FFT")
 plt.legend()
 plt.tight_layout()
 plt.grid()
-plt.show()"""
-#**********************************************************
-# 4.4
-frames2 = copy.deepcopy(frames)
-DFT = []
-DFT_log = []
-
-for k in range(0,87): # jednotlivé rámce
-    frames2[k] = np.pad(frames2[k], (0, 1024 - len(frames2[k])), 'constant')
-    N = len(frames2[k])
-    kof = np.fft.fft(frames2[k])
-    DFT.append(kof)
-    DFT_log.append(kof)
-    res_val = 10 * np.log10(np.abs(DFT[k])**2)
-    DFT_log[k] = res_val
-    DFT[k] = DFT[k][:512]
-    DFT_log[k] = DFT_log[k][:512]
-
-
-plt.figure(figsize=(12,6))
-plt.imshow(np.rot90(DFT_log), extent=[0, 1, 0, fs/2], aspect='auto')
-plt.gca().set_title('DFT')
-plt.gca().set_xlabel('t [s]')
-plt.gca().set_ylabel('f [Hz]')
-b = plt.colorbar()
-b.set_label('Spektralní hustota výkonu [dB]', rotation=270,labelpad=16)
-plt.tight_layout()
 plt.show()
 
+# 4.4
+f, t, sgr = spectrogram(data, fs)
+sgr_log = 10 * np.log10(sgr+1e-20) 
+
+plt.figure(figsize=(16,10))
+plt.pcolormesh(t,f,sgr_log)
+plt.gca().set_xlabel('Čas [s]')
+plt.gca().set_ylabel('Frekvence [Hz]')
+cbar = plt.colorbar()
+cbar.set_label('Spektralní hustota výkonu [dB]', rotation=270, labelpad=15)
+
+#4.5
+# zpozorovane skrz priblizeni spektogramu
+f = [1000,2000,3000,4000] #[Hz]
+# f1 985-1000
+# f2 1985-2000
+# f3 2985-3000
+# f4 3985-4000
+#4.6
+#source https://pythonnumericalmethods.berkeley.edu/notebooks/chapter24.02-Discrete-Fourier-Transform.html
+
+sr = fs
+ts = 1.0/fs
+t = np.arange(0,2.8288125,ts)
+sig_gen = 0
+for freq in f:
+    sig_gen += 2**8*np.cos(2*np.pi*freq*t)
+
+plt.figure(figsize=(6,3))
+plt.gca().set_title('4 cosinusovky na 32vzorcich')
+plt.plot(sig_gen[:32])
+plt.show()
+from scipy.io.wavfile import write
+write("../audio/4cos.wav", sr, sig_gen.astype(np.int16))
+
+f, t, sgr = spectrogram(sig_gen, fs)
+sgr_log = 10 * np.log10(sgr+1e-20) 
+
+plt.figure(figsize=(8,5))
+plt.pcolormesh(t,f,sgr_log)
+plt.gca().set_xlabel('Čas [s]')
+plt.gca().set_ylabel('Frekvence [Hz]')
+cbar = plt.colorbar()
+cbar.set_label('Spektralní hustota výkonu [dB]', rotation=270, labelpad=15)
+
+#4.7 pasmova zadrz
+from scipy import signal
+from scipy.signal import butter, lfilter
+
+
+def butter_bandstop(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='bandstop')
+    return b, a
+
+
+def butter_bandstop_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandstop(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+spectras = []
+filtry = []
+
+b1,a1 = butter_bandstop(985,1015,fs,5)
+filt1 = butter_bandstop_filter(data, 985 , 1015, fs)
+filtry.append(filt1)
+
+b2,a2 = butter_bandstop(1970,2000,fs,5)
+filt2 = butter_bandstop_filter(data, 1970, 2000, fs)
+filtry.append(filt2)
+
+b3,a3 = butter_bandstop(2970,3000,fs,5)
+filt3 = butter_bandstop_filter(data, 2970, 3000, fs)
+filtry.append(filt3)
+
+b4,a4 = butter_bandstop(3970,4000,fs,5)
+filt4 = butter_bandstop_filter(data, 3970, 4000, fs)
+filtry.append(filt4)
+
+"""
+for i in filtry:
+    f, t, sgr = spectrogram(i, fs)
+    sgr_log = 10 * np.log10(sgr+1e-20) 
+
+    plt.figure(figsize=(8,5))
+    plt.pcolormesh(t,f,sgr_log)
+    plt.gca().set_xlabel('Čas [s]')
+    plt.gca().set_ylabel('Frekvence [Hz]')
+    cbar = plt.colorbar()
+    cbar.set_label('Spektralní hustota výkonu [dB]', rotation=270, labelpad=15)
+"""
+
+
+
+aa = []
+bb = []
+aa.append(a1)
+aa.append(a2) 
+aa.append(a3) 
+aa.append(a4)
+
+bb.append(b1)
+bb.append(b2) 
+bb.append(b3) 
+bb.append(b4)
+
+N_imp = 64
+for i in range(4):
+
+    imp = [1, *np.zeros(N_imp-1)] # jednotkovy impuls
+    h = lfilter(bb[i], aa[i], imp)
+
+    plt.figure(figsize=(5,3))
+    plt.stem(np.arange(N_imp), h, basefmt=' ')
+    plt.gca().set_xlabel('$n$')
+    plt.gca().set_title('Impulsní odezva $h[n]$')
+    plt.grid(alpha=0.5, linestyle='--')
+    plt.tight_layout()
+    plt.show()
+
+#4.8
+for i in range(4):
+    z, p, k = tf2zpk(bb[i], aa[i])
+    plt.figure(figsize=(4,3.5))
+
+# jednotkova kruznice
+    ang = np.linspace(0, 2*np.pi,100)
+    plt.plot(np.cos(ang), np.sin(ang))
+
+    # nuly, poly
+    plt.scatter(np.real(z), np.imag(z), marker='o', facecolors='none', edgecolors='r', label='nuly')
+    plt.scatter(np.real(p), np.imag(p), marker='x', color='g', label='póly')
+
+    plt.gca().set_xlabel('Realná složka $\mathbb{R}\{$z$\}$')
+    plt.gca().set_ylabel('Imaginarní složka $\mathbb{I}\{$z$\}$')
+
+    plt.grid(alpha=0.5, linestyle='--')
+    plt.legend(loc='upper right')
+
+    plt.tight_layout()
+    plt.show()
